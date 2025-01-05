71abba6be9fdcbb8c2f6f2d25e669f894698e134
ZACKstrX <zakimatozi@gmail.com>
2025-01-05 16:26:05 +0100

    route for the  loginPost

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthManager extends Controller
 {
@@ -13,5 +14,15 @@ class AuthManager extends Controller
         return view('auth.login');
     }
 
-    function loginPost(Request $request) {}
+    function loginPost(Request $request) {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required|min:8',
+        ]);
+        $credentials = $request->only(['email', 'password']);
+        if(Auth::attempt($credentials)){
+            return redirect()->intended(route("home"));
+        }
+        return redirect("login")
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
     return view('welcome');
-});
+})->name("home");
 
 Route::get("/login",[AuthManager::class, "login"]);
+Route::post("/login",[AuthManager::class, "loginPost"])
+->name("loginPost");
